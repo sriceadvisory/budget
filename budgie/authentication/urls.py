426bdf2121fc337cd255b046613b452b3c42c565
srice704 <srice@legacyforcemedia.com>
2026-03-09 20:11:08 -0700

    Linked index.html to project

--- budgie/authentication/urls.py
+++ budgie/authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import RegisterView, LoginView, LogoutView
+from authentication.views import index
 
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
     path('auth/login/', LoginView.as_view(), name='login'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path('', index, name='index'),
 ]
