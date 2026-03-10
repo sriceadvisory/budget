b7a0a3339f4a0403c8da0d0a31ec1c3b90bfa74d
srice704 <srice@legacyforcemedia.com>
2026-03-09 20:12:50 -0700

    Linked URL for index frontend

--- budgie/budgie/urls.py
+++ budgie/budgie/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from authentication.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
     path('api/', include('authentication.urls')),
     path('api/', include('categories.urls')),
     path('api/', include('transactions.urls')),
