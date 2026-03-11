72326feb4dd724a98eea07fee204156c3300f4f2
srice704 <srice@legacyforcemedia.com>
2026-03-10 19:29:12 -0700

    Unregistering blacklist and outstanding tokens from admin

--- budgie/authentication/admin.py
+++ budgie/authentication/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.contrib.admin.sites import NotRegistered
+from django.apps import apps
+from django.utils.module_loading import import_module
 from .models import User
 
-# Register your models here.
 admin.site.register(User)
+
+# Ensure the token_blacklist app has a chance to register
+try:
+    import_module("rest_framework_simplejwt.token_blacklist.admin")
+except Exception:
+    pass
+
+# Unregister token models from admin 
+for model_name in ("OutstandingToken", "BlacklistedToken"):
+    try:
+        model = apps.get_model("token_blacklist", model_name)
+        admin.site.unregister(model)
+    except (LookupError, NotRegistered):
+        pass
