80cf637fd14872e59ca5cec8b040487343220b8a
srice704 <srice@legacyforcemedia.com>
2026-03-10 18:47:18 -0700

    Updating category urls to reflect categorydetailview

--- budgie/categories/urls.py
+++ budgie/categories/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import CategoryListCreateView, CategoryRetrieveUpdateDeleteView
+from .views import CategoryListCreateView, CategoryDetailView
 
 urlpatterns = [
     path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
-    path('categories/<uuid:pk>/', CategoryRetrieveUpdateDeleteView.as_view(), name='category-retrieve-update-delete'),
+    path('categories/<uuid:pk>/', CategoryDetailView.as_view(), name='category-retrieve-update-delete'),
 ]
