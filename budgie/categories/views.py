a660706ec0e30821b2184f111e9efe40bb7f2d01
srice704 <srice@legacyforcemedia.com>
2026-03-10 18:47:49 -0700

    Mapping out categories business logic

--- budgie/categories/views.py
+++ budgie/categories/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+from .filters import CategoryFilter
+
+
+class CategoryListCreateView(ListCreateAPIView):
+    serializer_class = CategorySerializer
+    filterset_class = CategoryFilter
+    pagination_class = PageNumberPagination
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    ordering_fields = ['name']
+    ordering = ['name']
+    search_fields = ['name']
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CategoryDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
